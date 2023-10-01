b7bb84512cf3fc86f132e48bd157e5933607180b
DAHAM-SENAVIRATHNA <nayanads2000@gmail.com>
2023-10-01 12:24:37 +0530

    Add pages and navbar component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,26 @@
 
 // Routes between various pages home, aboutus
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+
+//Pages
 import Home from "./pages/Home";
-function App() {
+import ArticleList from "./pages/ArticleList";
+import Article from "./pages/Article";
+import About from "./pages/About";
+
+// Components
+import Navbar from "./components/Navbar";
+
+const App = ()=> {
   return (
     <Router>
+      <Navbar />
       <div className="max-w-screen-md mx-auto pt-20">
         <Routes>
           <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/articles" element={<ArticleList />} />
+          <Route path="/article" element={<Article />} />
         </Routes>
       </div>
     </Router>
