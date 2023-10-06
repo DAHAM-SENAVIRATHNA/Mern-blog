394eb4a82b8027c2bbde3f1a892d5e6f187ea406
DAHAM-SENAVIRATHNA <nayanads2000@gmail.com>
2023-10-06 10:30:58 +0530

    Define route to retrieve data from mongodb

--- server.js
+++ server.js
@@ -1,24 +1,46 @@
+// Import dependencies
 const express = require('express');
-const app = express();
-PORT = process.env.PORT || 5000
+const { MongoClient } = require('mongodb');
 require('dotenv').config();
-const dbconfig = require('./db');
 
-// Initailize Middleware, function in express.. It parses incomming JSON payload => Alternative Body parser
-app.use(express.json({extended: false}))
+// Create Express app
+const app = express();
+const PORT = process.env.PORT || 5000;
+const uri = process.env.MONGODB_URI;
 
-app.get('api/articles/:name', (req,res) => {
-    
-})
+// Middleware for parsing JSON
+app.use(express.json());
 
-app.post('/api/articles/:name/add-comments', (req, res)=>{
-    const {username, text} = req.body
-    const articleName = req.params.name
-    articleInfo[articleName].comments.push({ username, text })
-    res.status(200).send(articleInfo[articleName]);
-    
-})
+// Connect to MongoDB
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 
-app.listen(PORT, ()=> {
-    console.log("Server started");
-})
+async function connectToMongoDB() {
+  try {
+    await client.connect();
+    console.log('Connected to MongoDB');
+  } catch (err) {
+    console.error('Error connecting to MongoDB:', err);
+  }
+}
+
+// Define a route to retrieve data from MongoDB
+app.get('/api/articles/:name', async (req, res) => {
+  const articleName = req.params.name;
+
+  const db = client.db('Mern_blog');
+  const collection = db.collection('articles');
+
+  try {
+    const articleInfo = await collection.findOne({ name: articleName });
+    res.status(200).json(articleInfo);
+  } catch (error) {
+    console.error('Error retrieving data from MongoDB:', error);
+    res.status(500).json({ message: 'Error retrieving data from MongoDB' });
+  }
+});
+
+// Start the server
+app.listen(PORT, () => {
+  connectToMongoDB(); // Connect to MongoDB when the server starts
+  console.log(`Server started on port ${PORT}`);
+});
